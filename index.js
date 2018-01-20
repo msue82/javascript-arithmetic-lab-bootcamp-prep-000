3d1ead57148b1d2fa2dd1c54da24ddfc5d66f3f0
Melissa Donahue <msue1282@gmail.com>
2018-01-20 03:15:53 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,49 @@
+function add(){
+return  a + b;
+
+
+}
+
+add(a, b);
+
+
+function subtract(){
+  return b - a;
+
+}
+
+subract(a,b);
+
+
+
+function multiply() {
+ return  a *= b;
+
+}
+multiply(a,b);
+
+
+function divide(){
+  return a /= b;
+
+}
+
+divide(a, b);
+
+
+function inc(n) {
+  return a+n;
+
+}
+inc(a, b);
+
+function dec(n){
+  return a-n;
+
+}
+dec();
+
+function makeInt(n) {
+  return parseInt( n, a);
+
+}
